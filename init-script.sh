9f87fcd2c0edb344246083faf37477896d64c214
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-04-21 13:15:44 +0530

    Update init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-git clone https://SmithaVerity:${GITHUB_TOKEN}@github.com/SmithaVerity/verityapp.git    
+git clone https://SmithaVerity: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@github.com/SmithaVerity/verityapp.git    
 cd verityapp/
 sh deployApp.sh
 sudo docker-compose up --build
