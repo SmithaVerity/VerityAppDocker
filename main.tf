f6ea81e99648fef02277e0546252990a80273ef6
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-04-22 20:07:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,9 +65,9 @@ resource "aws_instance" "verityapp" {
   associate_public_ip_address = lookup(var.awsprops, "publicip")
   key_name = lookup(var.awsprops, "keyname")
 
-  user_data = "${templatefile("init-script.sh"), {
+  user_data = ${templatefile("init-script.sh"), {
     GITHUB_TOKEN = "Smitha"
-  }}"
+  }}
 
   vpc_security_group_ids = [
     aws_security_group.verityappsg.id
